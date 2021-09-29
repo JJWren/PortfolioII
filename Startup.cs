b0f9fb1c816e4f9e0d81f06e82801ac44281586e
Joshua Wren <43586816+JJWren@users.noreply.github.com>
2021-09-29 13:28:05 -0500

    Impl. View pages  + some minor bootstrap structure

--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,7 @@ namespace PortfolioII
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => options.EnableEndpointRouting = false;)
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +27,7 @@ namespace PortfolioII
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseMvc();
         }
 
